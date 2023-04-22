934270467f5d557449fd71bb39e9174b8743d4e8
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2023-04-22 18:14:57 +0530

    Small Change made

---
+++ Backend/Entry/Login/verify_students.py
@@ -0,0 +1,17 @@
+import mysql.connector as sql
+
+conn = sql.connect(host='localhost', password='example-password',
+                                 user='abstract-programmer',
+                                 database = 'Student_Verification')
+
+cursor = conn.cursor()
+
+query = "SELECT * FROM Verify_Details WHERE Department = 'Computer'"
+cursor.execute(query)
+
+
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+conn.close()
