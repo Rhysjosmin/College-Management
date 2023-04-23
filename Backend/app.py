0b597f2004e2361228b6c0f1db7e3ed73176eae6
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-04-23 12:08:55 +0530

    Added Admin Page

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from Routes.Student.app import Student
-
+from Routes.Admin.app import Admin
 app=Flask(__name__)
 
 app.register_blueprint(Student,url_prefix='/Student')
+app.register_blueprint(Admin,url_prefix='/Admin')
+
 
 if __name__ =="__main__":
     app.run(debug=True)
