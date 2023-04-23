0a1b6ffb5a204f509b0fcb410db31197a47702bb
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-04-23 15:35:43 +0530

    View Mentees

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from Routes.Student.app import Student
 from Routes.Admin.app import Admin
 app=Flask(__name__)
-
+cors=CORS(app)
 app.register_blueprint(Student,url_prefix='/Student')
 app.register_blueprint(Admin,url_prefix='/Admin')
 
