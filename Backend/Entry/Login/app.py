59a670f4946a0dd5b38f56de5aeec11477138b1f
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-04-24 07:18:31 +0000

    Pending changes exported from your codespace

--- Backend/Entry/Login/app.py
+++ Backend/Entry/Login/app.py
@@ -3,9 +3,23 @@ import mysql.connector
 
 Login=Blueprint('Login',__name__)
 
-@Login.route('/<EncryptedUsername>/<EncryptedPassword/<Key>')
-def Login():
-    
-    if User in DB:
-        return 'Pass'
-    return 'Fail'
+
+@Login.route('/<StudentName>/<RollNumber>/', methods=['GET'])
+def Login(StudentName, RollNumber):
+    conn = SQL.connect(host='localhost', password='example-password',
+                                 user='abstract-programmer',
+                                 database = 'Student_Verification')
+
+    cursor = conn.cursor()
+
+    query = f"SELECT * FROM Verify_Details WHERE StudentName = {StudentName} AND RollNumber = {RollNumber} "
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    if result:
+         return({'found':True})
+
+    else:
+        return({'found':False})
+
+        
